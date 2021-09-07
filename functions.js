95cd38349e1392ab59f60bffa1fd421ec8fda42c
ginnygu <ginnygu@gmail.com>
2021-09-07 14:58:39 -0400

    initial commit

---
+++ functions.js
@@ -0,0 +1,62 @@
+const returnEmptyString = function () {};
+
+const returnZeroNumber = function () {};
+
+const returnEmptyArray = function () {};
+
+const returnEmptyObject = function () {};
+
+const returnAbcString = function (value) {};
+
+const returnSumOfTen = function (arg1, arg2) {};
+
+const subtraction = function (arg1, arg2) {};
+
+const multiplication = function (arg1, arg2) {};
+
+const division = function (arg1, arg2) {};
+
+const returnArray = function (arg1) {};
+
+const returnFirstIndex = function (arg1) {};
+
+const returnSecondIndex = function (arg1) {};
+
+const returnArrayLength = function (arg1) {};
+
+const arraySum = function (arg1) {};
+
+const arraySubtraction = function (arg1) {};
+
+const multiplicationArray = function (arg1) {};
+
+const divisionArray = function (arg1) {};
+
+const oddArray = function (arg1) {};
+
+const evenArray = function (arg1) {};
+
+const returnFruits = function (arg1) {};
+
+module.exports = {
+  returnEmptyArray,
+  returnEmptyString,
+  returnZeroNumber,
+  returnEmptyObject,
+  returnAbcString,
+  returnSumOfTen,
+  subtraction,
+  multiplication,
+  division,
+  returnArray,
+  returnFirstIndex,
+  returnSecondIndex,
+  returnArrayLength,
+  arraySum,
+  arraySubtraction,
+  multiplicationArray,
+  divisionArray,
+  oddArray,
+  evenArray,
+  returnFruits,
+};
